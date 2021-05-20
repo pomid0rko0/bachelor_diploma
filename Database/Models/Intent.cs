4b919b87321e3be374bee9cbb8d8d3b54d3120da
Egor Sychev <Lev55i@ya.ru>
2021-05-21 04:30:01 +0700

    anottate fields

--- Database/Models/Intent.cs
+++ Database/Models/Intent.cs
@@ -9,7 +9,9 @@ namespace Database.Models
         public int Id { set; get; }
         [Required]
         public string Name { set; get; }
+        [Required]
         public virtual ICollection<Question> Questions { set; get; }
+        [Required]
         public virtual ICollection<Answer> Answers { set; get; }
 
 #nullable enable
